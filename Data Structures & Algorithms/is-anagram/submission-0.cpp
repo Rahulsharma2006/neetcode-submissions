46b95c237a5013080b89e59fb142088c188fc8e4
Rahul Sharma <rs0987jrkg@gmail.com>
2026-08-01 20:41:39 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>mp;
+        for(char ch : s){
+            mp[ch]++;
+        }
+        for(char ch : t){
+            mp[ch]--;
+            if(mp[ch]==0){
+                mp.erase(ch);
+            }
+        }
+        if(mp.size()>0)return false;
+
+        return true;
+    }
+};
