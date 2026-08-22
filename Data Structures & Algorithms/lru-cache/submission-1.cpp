4a5bfecc26a0e92b9066f2efc5bd2837a7797623
Rahul Sharma <rs0987jrkg@gmail.com>
2026-08-22 08:15:14 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.cpp
@@ -0,0 +1,66 @@
+class LRUCache {
+public:
+class Node{
+    public:
+    int key , val;
+    Node* prev;
+    Node* next;
+    Node(int k , int v){
+        key = k;
+        val = v;
+        prev=next=NULL;
+    }
+};
+    Node * head = new Node(-1,-1);
+    Node * tail = new Node(-1,-1);
+    unordered_map<int,Node*>mp;
+    int limit;
+    LRUCache(int capacity) {
+        limit = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+       if(mp.find(key)==mp.end()){
+        return -1;
+       }
+      int  ans = mp[key]->val;
+      Node* ansNode = mp[key];
+       mp.erase(key);
+       deleteNode(ansNode);
+       addNode(ansNode);
+       mp[key]=ansNode;
+       return ans;
+    }
+    
+    void put(int key, int value) {
+        Node* newNode = new Node(key,value);
+        if(mp.find(key)!=mp.end()){
+            Node* temp = mp[key];
+            mp.erase(key);
+            deleteNode(temp);
+        }
+        if(mp.size()==limit){
+            Node* temp=tail->prev;
+            mp.erase(temp->key);
+            deleteNode(temp);
+            delete(temp);
+        }
+        addNode(newNode);
+        mp[key]=newNode;
+    }
+    void deleteNode(Node * oldNode){
+        Node *oldprev = oldNode->prev;
+       Node * oldNext = oldNode->next;
+        oldprev->next = oldNext;
+        oldNext->prev = oldprev;
+    }
+    void addNode(Node* newNode){
+        Node* temp = head->next;
+        head ->next = newNode;
+        temp->prev = newNode;
+        newNode->next = temp;
+        newNode->prev = head;
+    }
+};
